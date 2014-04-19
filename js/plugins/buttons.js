0d5b1314de88c179386541c28068e2f8d1aae694
No3x <noex_home@yahoo.de>
2014-04-20 01:54:07 +0200

    gh-25 Buttons migration for WP 3.9

--- js/plugins/buttons.js
+++ js/plugins/buttons.js
@@ -1,70 +1,20 @@
-// JavaScript Document
 (function() {
-    // Creates a new plugin class and a custom listbox
-    tinymce.create('tinymce.plugins.bs_buttons', {
-        createControl: function(n, cm) {
-            switch (n) {                
-                case 'bs_buttons':
-                var c = cm.createSplitButton('bs_buttons', {
-                    title : 'Buttons',
-                    onclick : function() {
-
-                    }
-                    //'class':'mceListBoxMenu'
-                });
-                
-
-                c.onRenderMenu.add(function(c, m) {
-                    m.onShowMenu.add(function(c,m){
-                        jQuery('#menu_'+c.id).height('auto').width('auto');
-                        jQuery('#menu_'+c.id+'_co').height('auto').addClass('mceListBoxMenu'); 
-                        var $menu = jQuery('#menu_'+c.id+'_co').find('tbody:first');
-                        if($menu.data('added')) return;
-                        $menu.append('');
-                        $menu.append('<div style="padding: 0 10px 10px"><label>Size<br/>\
-                        <select name="size">\
-                        <option value="xs">Mini</option>\
-                        <option value="sm">Small</option>\
-                        <option value="Normal" selected>Normal</option>\
-                        <option value="lg">Large</option>\
-                        </select></label>\
-                        <label>Types<br/>\
-                        <select name="type">\
-                        <option value="Default"> Default</option>\
-                        <option value="Primary"> Primary</option>\
-                        <option value="Success"> Success</option>\
-                        <option value="Info" selected> Info</option>\
-                        <option value="Warning"> Warning</option>\
-                        <option value="Danger"> Danger</option>\
-                        <option value="Link"> Link</option>\
-                        </select>\
-                        <label>Link<br />\
-                        <input type="text" name="link" value="#" onclick="this.select()"  /></label>\
-                        </div>');
-
-                        jQuery('<input type="button" class="button" value="Insert" />').appendTo($menu)
-                                .click(function(){
-                                    var size = $menu.find('select[name=size]').val();
-                                    var type = $menu.find('select[name=type]').val();
-                                    var link = $menu.find('input[name=link]').val();
-                                    tinymce.activeEditor.execCommand('mceInsertContent',false,'[bs_button size="'+size.toLowerCase()+'" type="'+type.toLowerCase()+'" value="'+type+'" href="'+link+'"]');
-                                    c.hideMenu();
-                                }).wrap('<div style="padding: 0 10px 10px"></div>')
-                 
-                        $menu.data('added',true); 
-
-                    });
-
-                   // XSmall
-                    m.add({title : 'Buttons', 'class' : 'mceMenuItemTitle'}).setDisabled(1);
-
-                 });
-                // Return the new splitbutton instance
-                return c;
-                
-            }
-            return null;
-        }
+	// Creates a new plugin
+    tinymce.PluginManager.add( 'bs_buttons', function( editor, url ) {
+        editor.addButton( 'bs_buttons', { 
+        	text: 'Buttons',
+        	tooltip: tinymce.translate('Add a button'),
+            icon: false,
+            onclick: function() {
+            	tinymce.activeEditor.windowManager.open({
+            	   title: 'Buttons',
+        		   url: url + '/buttons.php',
+             	   width: 480,
+             	   height: 320
+             	}, {
+             	   custom_param: 1
+             	});
+            } 
+        });
     });
-    tinymce.PluginManager.add('bs_buttons', tinymce.plugins.bs_buttons);
 })();
---
+++ js/plugins/buttons.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="stylesheet" href="../../css/bootstrap.css" />
+<link rel="stylesheet" href="../../css/admin.css" />
+<script type="text/javascript" src="../jquery.js"></script>
+<script type="text/javascript" src="../bootstrap.js"></script>
+<script type="text/javascript">
+$(function() {
+	jQuery('').appendTo("body").click(function() {
+        var size = jQuery('body').find('select[name=size]').val();
+        var type = jQuery('body').find('select[name=type]').val();
+        var link = jQuery('body').find('input[name=link]').val();
+        top.tinymce.activeEditor.insertContent('[bs_button size="'+size.toLowerCase()+'" type="'+type.toLowerCase()+'" value="'+type+'" href="'+link+'"]');
+        top.tinymce.activeEditor.windowManager.close();
+	}).wrap('<div style="padding: 10px"></div>');
+});
+</script>
+</head>
+<body>
+	<div style="padding: 10px">
+		<form role="form">
+			<div class="form-group">
+				<label for="size">Size</label>
+					<select class="form-control" id="size" name="size">
+						<option value="xs">Mini</option>
+						<option value="sm">Small</option>
+						<option value="md" selected>Normal</option>
+						<option value="lg">Large</option>
+					</select>
+				<label for="type">Types</label>
+				<select class="form-control" id="type" name="type">
+					<option value="Default"> Default</option>
+					<option value="Primary"> Primary</option>
+					<option value="Success"> Success</option>
+					<option value="Info" selected> Info</option>
+					<option value="Warning"> Warning</option>
+					<option value="Danger"> Danger</option>
+					<option value="Link"> Link</option>
+				</select>
+				<label for="link">Link</label>
+				<input type="text" class="form-control" id="link" name="link" placeholder="#" onclick="this.select()" />
+			</div>
+			<button type="submit" class="btn btn-primary">Insert</button>
+		</form>
+	</div>
+</body>
+</html>
