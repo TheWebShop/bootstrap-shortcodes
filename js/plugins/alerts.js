95edda62c76cb955fc7a1465c1aabd4c53797feb
No3x <noex_home@yahoo.de>
2014-04-20 13:07:36 +0200

    gh-25 Alerts migration for WP 3.9 - fixed argument list

--- js/plugins/alerts.js
+++ js/plugins/alerts.js
@@ -11,8 +11,7 @@
                { text: 'Info notification', onclick: function() { editor.insertContent('[bs_notification type="info"]<strong>Heads up!</strong>This <a href="#" class="alert-link">alert needs your attention</a>, but it\'s not super important.[/bs_notification]');} },
                { text: 'Warning notification', onclick: function() { editor.insertContent('[bs_notification type="warning"]<strong>Warning!</strong>Best check yo self, you\'re <a href="#" class="alert-link">not looking too good</a>.[/bs_notification]');} },
                { text: 'Error notification', onclick: function() { editor.insertContent('[bs_notification type="danger"]<strong>Oh snap!</strong> <a href="#" class="alert-link">Change a few things</a> up and try submitting again.[/bs_notification]');} }
-           ],
-         
+           ]
         });
     });
 })();
